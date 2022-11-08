360221073211f5777aebc46a6108b755b84b65bf
Precious Ighereno <preciousighereno@gmail.com>
2022-11-08 15:29:05 +0100

    created templates

--- buddyapp/buddyapp/urls.py
+++ buddyapp/buddyapp/urls.py
@@ -5,6 +5,25 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name = 'homepage'),
-    path('accounts/', include('allauth.urls')),
     
+    #registration
+    path('accounts/', include('allauth.urls')),
+
+    path('register_user/',views.register_user, name = 'signup'),
+    #signup
+    path('accounts/signup/',views.signup_user, name = 'signup'),
+    #signedup check your mailbox
+
+    #verify your mail
+
+    #email verified
+
+    #login
+    path('accounts/login/',views.login_user, name = 'login'),
+    path("logout_user/",views.logout_user, name = 'logout'),
+     #dashboard
+    path('dashboard/',views.dashboard, name = 'dashboard'),
 ]
+#handler404 = "buddyapp.views.handle_not_found"
+    
+
--- buddyapp/buddyapp/views.py
+++ buddyapp/buddyapp/views.py
@@ -1,5 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required 
 
 def homepage (request):
     return render (request, 'homepage.html', {})
+
+def register_user (request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('login')
+    else:
+        form = CreateUserForm()
+    context = {
+                'form':form
+                }
+    return render (request, 'registerPage.html', context)
+
+def login_user (request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('dashboard')
+        
+    else:
+        # Return an 'invalid login' error message.
+        form = CreateUserForm()
+    context = {
+                'form':form
+                }
+    return render (request, 'loginPage.html', context)
+def signup_user (request):
+    return render (request, 'registerPage.html')
+
+def dashboard(request):
+    return render (request, 'dashboard.html')
+
+def logout_user (request):
+        logout (request)
+        return redirect("login")
