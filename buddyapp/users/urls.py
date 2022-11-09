6179463905751e7b4212748560544a4a8993a0e8
Precious Ighereno <preciousighereno@gmail.com>
2022-11-09 02:27:44 +0100

    Login, Register, and Logout

--- buddyapp/users/urls.py
+++ buddyapp/users/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from knox import views as knox_views
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_api),
+    path('accounts/login/', views.login_api),
+    path('accounts/user/', views.get_user_data),
+    path('accounts/register/', views.register_api),
+    path('accounts/logout/', knox_views.LogoutView.as_view()),
+    path('accounts/logoutall/', knox_views.LogoutView.as_view()),
 ]
--- buddyapp/users/views.py
+++ buddyapp/users/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.auth import AuthToken
+from .serializers import RegisterSerializer
 
 @api_view(['POST'])
 def login_api(request):
@@ -19,7 +20,36 @@ def login_api(request):
          },
           'token': token
     })
-       
+
+@api_view(['GET'])
+def get_user_data(request):
+    user = request.user
+
+    if user.is_authenticated:
+        return Response({
+            'user_info': {
+                'id': user.id,
+                'username': user.username,
+                'email': user.email,
+            }
+        })
+    
+    return Response({'error': 'not authenticated'}, status=400)
             
-       
+@api_view(['POST'])
+def register_api(request):
+    serializer =RegisterSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    user = serializer.save()    
+    _, token = AuthToken.objects.create(user)
+
+    return Response({
+         'user_info': {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+         },
+          'token': token
+    })
  
